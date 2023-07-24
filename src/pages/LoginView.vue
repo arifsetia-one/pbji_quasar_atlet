f04770a7c64abbb284200c3173de56026c54ca68
ArifSetiawan <arif.setiawan2209@students.amikom.c.id>
2023-07-24 21:13:23 +0700

    add target _blank on login admin

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -1,47 +1,28 @@
 <template>
   <!-- form login -->
   <section>
-    <div
-      class="flex items-center justify-center min-h-screen bg-gray-100 pr-4 pl-4"
-    >
+    <div class="flex items-center justify-center min-h-screen bg-gray-100 pr-4 pl-4">
       <div class="px-10 py-8 text-left bg-white shadow-lg">
         <h3 class="text-2xl mt-4 font-bold text-center">Masuk ke Akun Atlet</h3>
         <form @submit.prevent="onSubmit">
           <div class="mt-4 sm:w-96">
             <div>
               <label class="block" for="email">Username</label>
-              <input
-                v-model="inputData.username"
-                type="text"
-                id="username"
-                placeholder="Username"
-                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
-              />
+              <input v-model="inputData.username" type="text" id="username" placeholder="Username"
+                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" />
             </div>
             <div class="mt-4">
               <label class="block">Password</label>
-              <input
-                v-model="inputData.password"
-                type="password"
-                id="password"
-                placeholder="Password"
-                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600"
-              />
+              <input v-model="inputData.password" type="password" id="password" placeholder="Password"
+                class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-red-600" />
             </div>
-            <a
-              href="http://localhost:9001/#/auth/login"
-              id="loginAdmin"
-              class="flex justify-center text-sm mt-4 font-medium text-red-600 hover:underline"
-              >Login sebagai admin</a
-            >
+            <a href="http://localhost:9001/" id="loginAdmin"
+              class="flex justify-center text-sm mt-4 font-medium text-red-600 hover:underline" target="_blank">Login
+              sebagai
+              admin</a>
             <div class="flex items-baseline justify-between">
-              <button
-                type="submit"
-                class="w-full px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-900"
-                id="login"
-                :disable="submitting"
-                :loading="submitting"
-              >
+              <button type="submit" class="w-full px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-900"
+                id="login" :disable="submitting" :loading="submitting">
                 Login
               </button>
             </div>
